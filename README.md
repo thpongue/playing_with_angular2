763ac713050107065f185341b30490fc930a0f5d
Dominic McAdden <dominic.mcadden@gmail.com>
2016-07-11 14:22:32 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Playing with Angular 2 - this is based off the quickstart at https://angular.io/docs/ts/latest/quickstart.html
---
+++ app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>playing with angular 2</h1>'
+})
+export class AppComponent { }
